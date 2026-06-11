e846d98780e2a137a922e1f67c783eb2d5edea12
Shreyansh Shukla <shreyanshshukla1056@gmail.com>
2026-06-11 10:26:39 +0530

    Add files via upload

---
+++ WEEK_1/DAY_1/Q4_Count_Digits.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    int count = 0;
+    while(n > 0){
+        count++;
+        n /= 10;
+    }
+    cout << count;
+    return 0;
+}
